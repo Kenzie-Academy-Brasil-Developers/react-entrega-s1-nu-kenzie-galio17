247e5d0fdfbcac94f915086360d54fafbbeafc90
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-07-22 19:03:07 -0300

    feat: add Filter component

--- src/components/Dashboard/Main/index.jsx
+++ src/components/Dashboard/Main/index.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
+import Filter from "./Filter";
 import Form from "./Form";
 import List from "./List";
 import TotalMoney from "./TotalMoney";
 
 function Main() {
   const [transactionList, setTransactionList] = useState([]);
-  useEffect(() => setDisplayList(transactionList));
-
   const [displayList, setDisplayList] = useState(transactionList);
+  const [followDatabase, setFollowDatabase] = useState("");
+
+  useEffect(() =>
+    followDatabase.length
+      ? setDisplayList(displayList)
+      : setDisplayList(transactionList)
+  );
 
   return (
     <main>
@@ -17,6 +23,11 @@ function Main() {
           <TotalMoney displayList={displayList} />
         </section>
         <section>
+          <Filter
+            setDisplayList={setDisplayList}
+            transactionList={transactionList}
+            setFollowDatabase={setFollowDatabase}
+          />
           <List
             displayList={displayList}
             setTransactionList={setTransactionList}
