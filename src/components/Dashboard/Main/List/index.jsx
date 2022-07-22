5e846e6d75f749b10cc1eec3d4a79cec629cdeee
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-07-22 14:47:33 -0300

    feat: add List component

---
+++ src/components/Dashboard/Main/List/index.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card";
+
+function List({ displayList, setTransactionList }) {
+  const liItems = displayList.map((transaction) => (
+    <Card
+      key={transaction.id}
+      setTransactionList={setTransactionList}
+      transaction={transaction}
+    />
+  ));
+
+  return <ul>{liItems}</ul>;
+}
+
+export default List;
