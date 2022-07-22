f3684f16009aef291170dffe639f0d2ddbb1d3a7
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-07-22 13:10:11 -0300

    feat: add transaction database + add props to Form component

--- src/components/Dashboard/Main/index.jsx
+++ src/components/Dashboard/Main/index.jsx
@@ -1,11 +1,17 @@
+import React, { useState, useEffect } from "react";
 import Form from "./Form";
 
 function Main() {
+  const [transactionList, setTransactionList] = useState([]);
+  useEffect(() => setDisplayList(transactionList));
+
+  const [displayList, setDisplayList] = useState(transactionList);
+
   return (
     <main>
       <div className="container">
         <section>
-          <Form />
+          <Form setTransactionList={setTransactionList} />
         </section>
         <section></section>
       </div>
