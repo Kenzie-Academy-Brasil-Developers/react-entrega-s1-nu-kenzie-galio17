9562c3517f9134273c398988b0052c791ca8491c
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-07-22 14:47:00 -0300

    feat: add Card component

---
+++ src/components/Dashboard/Main/List/Card/index.jsx
@@ -0,0 +1,23 @@
+function Card({
+  setTransactionList,
+  transaction: { id, description, value, typeValue },
+}) {
+  function removeTransaction() {
+    setTransactionList((transactionList) =>
+      transactionList.filter((newTransaction) => newTransaction.id !== id)
+    );
+  }
+
+  return (
+    <li>
+      <div>
+        <h3>{description}</h3>
+        <span>{typeValue}</span>
+      </div>
+      <h4>{value}</h4>
+      <button onClick={removeTransaction}>lixeira</button>
+    </li>
+  );
+}
+
+export default Card;
