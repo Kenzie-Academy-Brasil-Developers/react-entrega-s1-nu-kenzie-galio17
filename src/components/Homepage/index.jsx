1a62d0e9572e334f4af7428e52773de18b8e9c65
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-07-25 16:57:39 -0300

    feat: style homepage

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,4 +1,6 @@
 import ilustration from "../../assets/img/ilustration.svg";
+import Button from "../Global/Button/styles";
+import Container from "./styles";
 
 function Homepage({ setIsLoggedIn }) {
   function handleLogin() {
@@ -6,24 +8,28 @@ function Homepage({ setIsLoggedIn }) {
   }
 
   return (
-    <main>
-      <section>
-        <h1 className="logo">
-          <strong>Nu</strong> Kenzie
-        </h1>
-        <p>Centralize o controle das suas finanças</p>
-        <span>de forma rápida e segura</span>
-        <button onClick={handleLogin}>Iniciar</button>
-      </section>
-      <div>
-        <div>
+    <Container>
+      <main>
+        <section className="lobby">
+          <h1>
+            <strong>Nu</strong> Kenzie
+          </h1>
+          <p>Centralize o controle das suas finanças</p>
+          <span>de forma rápida e segura</span>
+          <Button primary onClick={handleLogin}>
+            Iniciar
+          </Button>
+        </section>
+        <section className="ilustration">
           <div>
-            <div></div>
+            <div>
+              <div></div>
+            </div>
           </div>
-        </div>
-        <img src={ilustration} alt="Ilustração" />
-      </div>
-    </main>
+          <img src={ilustration} alt="Ilustração" />
+        </section>
+      </main>
+    </Container>
   );
 }
 
